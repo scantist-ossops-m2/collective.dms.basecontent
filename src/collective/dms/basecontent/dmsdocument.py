9bdbdbe26fb22d438f1a2296a5d496c98713d147
Gauthier Bastien <g.bastien@imio.be>
2013-01-08 15:06:09 +0100

    Use LocalRolesToPrincipals

--- src/collective/dms/basecontent/dmsdocument.py
+++ src/collective/dms/basecontent/dmsdocument.py
@@ -1,8 +1,5 @@
 from zope import schema
-from zope.component import adapts
 from zope.interface import implements
-
-from plone.app.content.interfaces import INameFromTitle
 
 from plone.dexterity.content import Container
 from plone.dexterity.schema import DexteritySchemaPolicy
@@ -10,6 +7,7 @@ from plone.dexterity.schema import DexteritySchemaPolicy
 from plone.supermodel import model
 
 from . import _
+from ._field import LocalRolesToPrincipals
 
 
 class IDmsDocument(model.Schema):
@@ -17,7 +15,19 @@ class IDmsDocument(model.Schema):
 
     notes = schema.Text(
         title=_(u"Notes"),
-        required=False
+        required=False,
+        )
+
+    treating_groups = LocalRolesToPrincipals(
+        title=_(u"Treating groups"),
+        required=False,
+        roles_to_assign=('Editor',)
+        )
+
+    recipient_groups = LocalRolesToPrincipals(
+        title=_(u"Recipient groups"),
+        required=False,
+        roles_to_assign=('Reader',)
         )
 
 class DmsDocument(Container):
