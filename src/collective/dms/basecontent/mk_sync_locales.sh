1d6d697b088edf07a0da5d8ff9c195e583127ac0
Stephan Geulette <s.geulette@imio.be>
2013-01-09 10:36:06 +0100

    separate po initialization and sync

--- src/collective/dms/basecontent/mk_sync_locales.sh
+++ src/collective/dms/basecontent/mk_sync_locales.sh
@@ -26,40 +26,44 @@ for pot in $(find locales -mindepth 1 -maxdepth 1 -type f -name "*.pot" ! -name 
     #finding pot basename as catalog
     catalog=`basename $pot .pot`
     echo "=> Found pot $pot"
-    # Compile po files
+    # First initialization of po files with base languages if po doesn't exist
     for language in $LANGUAGES; do
         arr=(`echo $language | cut -d "|"  --output-delimiter=" " -f 1-`)
         langcode=${arr[0]}
         install -d locales/$langcode/LC_MESSAGES
 
-        if test -d locales/$langcode/LC_MESSAGES; then
-    
-            PO=locales/$langcode/LC_MESSAGES/$catalog.po
-            # Create po file if not exists
-            if ! test -f $PO; then
-                touch $PO
-                echo " -> Syncing $PO"
-                i18ndude sync --pot $pot $PO
-                sed -i -e "/^\\\"Domain: DOMAIN/ s/DOMAIN/$catalog/" $PO
-                sed -i -e "/^\\\"Language-Code: en/ s/en/$langcode/" $PO
-                langname=${arr[1]}
-                if [ -n "$langname" ]; then
-                    sed -i -e "/^\\\"Language-Name: English/ s/English/$langname/" $PO
-                fi
-                fallbackstr=${arr[2]}
-                if [ -n "$fallbackstr" ]; then
-                    fallbacklist=`echo $fallbackstr | cut -d ";"  --output-delimiter=" " -f 1-`
-                    echo $fallbacklist
-                    sed -i -e "/^\\\"Language-Name:/ a\"X-is-fallback-for: $fallbacklist\\\n\"" $PO
-                fi
-            else
-                i18ndude sync --pot $pot $PO
+        PO=locales/$langcode/LC_MESSAGES/$catalog.po
+        # Create po file if not exists and modify header
+        if ! test -f $PO; then
+            touch $PO
+            echo " -> Syncing $PO"
+            i18ndude sync --pot $pot $PO
+            sed -i -e "/^\\\"Domain: DOMAIN/ s/DOMAIN/$catalog/" $PO
+            sed -i -e "/^\\\"Language-Code: en/ s/en/$langcode/" $PO
+            langname=${arr[1]}
+            if [ -n "$langname" ]; then
+                sed -i -e "/^\\\"Language-Name: English/ s/English/$langname/" $PO
             fi
-    
-            # Compile .po to .mo (msgfmt is in package gettext)
-            MO=locales/$langcode/LC_MESSAGES/$catalog.mo
+            fallbackstr=${arr[2]}
+            if [ -n "$fallbackstr" ]; then
+                fallbacklist=`echo $fallbackstr | cut -d ";"  --output-delimiter=" " -f 1-`
+                echo $fallbacklist
+                sed -i -e "/^\\\"Language-Name:/ a\"X-is-fallback-for: $fallbacklist\\\n\"" $PO
+            fi
+        fi
+    done
+    # Sync po files
+    for lang in $(find locales -mindepth 1 -maxdepth 1 -type d); do
+        if test -d $lang/LC_MESSAGES; then
+            PO=$lang/LC_MESSAGES/$catalog.po
+            # Sync po file
+            echo " -> Syncing $PO"
+            i18ndude sync --pot $pot $PO
+
+            # Compile .po to .mo
+            MO=$lang/LC_MESSAGES/$catalog.mo
             #echo " -> Compiling $MO"
-            #msgfmt -o $MO locales/$lang/LC_MESSAGES/$catalog.po
+            #msgfmt -o $MO $lang/LC_MESSAGES/$catalog.po
         fi
     done
 done
