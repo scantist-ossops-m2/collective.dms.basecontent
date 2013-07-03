9758ddcb43e112b419497e08d305a213116a2235
Cédric Messiant <cedricmessiant@ecreall.com>
2013-07-03 18:07:39 +0200

    No longer use IBasic behavior for DmsFile, title is the computed version number

--- src/collective/dms/basecontent/dmsfile.py
+++ src/collective/dms/basecontent/dmsfile.py
@@ -1,20 +1,35 @@
+from BTrees.Length import Length
+
+from five import grok
+
+from zope import schema
+from zope.annotation.interfaces import IAnnotations
 from zope.interface import implements
 
+from plone import api
+from plone.autoform import directives as form
 from plone.dexterity.content import Item
 from plone.dexterity.schema import DexteritySchemaPolicy
-
+from plone.directives.form import default_value
 from plone.namedfile.field import NamedBlobFile
 from plone.supermodel import model
 from plone.app.contenttypes.interfaces import IFile
 
 from . import _
+from zope.lifecycleevent.interfaces import IObjectAddedEvent
 
 
 class IDmsFile(model.Schema, IFile):
     """Schema for DmsFile"""
+    title = schema.TextLine(
+        title=_(u'Version number'),
+        required=True
+    )
+    form.mode(title='hidden')
+
     model.primary('file')
     file = NamedBlobFile(
-        title=_("File"),
+        title=_(u"File"),
         required=True,
     )
 
@@ -29,3 +44,24 @@ class DmsFileSchemaPolicy(DexteritySchemaPolicy):
 
     def bases(self, schemaName, tree):
         return (IDmsFile, )
+
+
+@default_value(field=IDmsFile['title'])
+def titleDefaultValue(data):
+    catalog = api.portal.get_tool('portal_catalog')
+    container = data.context
+    annotations = IAnnotations(container)
+    if 'higher_version' not in annotations:
+        version_number = 1
+    else:
+        version_number = annotations['higher_version'].value
+    return unicode(version_number)
+
+
+@grok.subscribe(IDmsFile, IObjectAddedEvent)
+def update_higher_version(context, event):
+    container = context.getParentNode()
+    annotations = IAnnotations(container)
+    if 'higher_version' not in annotations:
+        annotations['higher_version'] = Length()
+    annotations['higher_version'].change(1)
