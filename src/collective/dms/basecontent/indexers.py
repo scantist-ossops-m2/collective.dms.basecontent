fd670646e7ac1c2af16daaa31456ffa73c9df9fb
Frédéric Péters <fpeters@entrouvert.com>
2013-07-11 17:30:53 +0200

    rewrote full text indexing to consider latest version and appendix files

--- src/collective/dms/basecontent/indexers.py
+++ src/collective/dms/basecontent/indexers.py
@@ -7,31 +7,35 @@ from .dmsdocument import IDmsDocument
 
 @indexer(IDmsDocument)
 def document_dynamic_searchable_text_indexer(obj):
-    # XXX: this hardcodes the version created by the batchimport process; it
-    # should instead look into the content and take the latest version.
-    try:
-        data = obj['main'].file
-    except KeyError:
-        data = None
-
-    if not data or data.getSize() == 0:
-        return obj.title
+    indexed_elements = [obj.title]
 
     # if there is no path to text/plain, do nothing
     transforms = getToolByName(obj, 'portal_transforms')
-    if not transforms._findPath(data.contentType, 'text/plain'):
-        return obj.title
 
-    # convert it to text/plain
-    try:
-        datastream = transforms.convertTo(
-            'text/plain', data.data, mimetype=data.contentType,
-            filename=data.filename)
-        return obj.title + ' ' + unicode(datastream.getData(), 'utf-8')
-    except (ConflictError, KeyboardInterrupt):
-        raise
+    had_version = False
+    for child in reversed(obj.values()):
+        if child.portal_type in ('dmsmainfile', 'dmsappendixfile'):
+            if not child.file or child.file.getSize() == 0:
+                continue
 
-    return obj.title
+            if not transforms._findPath(child.file.contentType, 'text/plain'):
+                continue
+
+            # only index the latest version
+            if child.portal_type == 'dmsmainfile' and had_version:
+                continue
+            had_version = True
+
+            # convert it to text/plain
+            try:
+                datastream = transforms.convertTo(
+                    'text/plain', child.file.data, mimetype=child.file.contentType,
+                    filename=child.file.filename)
+                indexed_elements.append(unicode(datastream.getData(), 'utf-8'))
+            except (ConflictError, KeyboardInterrupt):
+                raise
+
+    return u' '.join(indexed_elements)
 
 grok.global_adapter(document_dynamic_searchable_text_indexer,
                     name='SearchableText')
