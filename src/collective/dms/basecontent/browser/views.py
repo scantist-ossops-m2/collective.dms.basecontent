b2a4e087ee249e3100d67d83506e5891804f7351
Stephan Geulette <s.geulette@imio.be>
2014-03-27 14:34:52 +0100

    Add a dmsdocument edit view including documentviewer to complete attributes when reading document.

--- src/collective/dms/basecontent/browser/views.py
+++ src/collective/dms/basecontent/browser/views.py
@@ -1,8 +1,9 @@
 import json
-
 from zope.component import getMultiAdapter
 from collective.documentviewer.views import DocumentViewerView
 from plone.dexterity.browser.view import DefaultView
+from plone.dexterity.browser.edit import DefaultEditForm
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
 class VersionViewerView(DocumentViewerView):
@@ -19,6 +20,15 @@ class DmsDocumentView(DefaultView):
     def update(self):
         super(DmsDocumentView, self).update()
         self.portal_url = getMultiAdapter((self.context, self.request),
-            name="plone_portal_state").portal_url()
-        self.dvstatic = "%s/++resource++dv.resources" % (
-            self.portal_url)
+                                          name="plone_portal_state").portal_url()
+        self.dvstatic = "%s/++resource++dv.resources" % (self.portal_url)
+
+
+class DmsDocumentEdit(DefaultEditForm):
+    template = ViewPageTemplateFile('templates/dmsdocument_edit.pt')
+
+    def update(self):
+        super(DmsDocumentEdit, self).update()
+        self.portal_url = getMultiAdapter((self.context, self.request),
+                                          name="plone_portal_state").portal_url()
+        self.dvstatic = "%s/++resource++dv.resources" % (self.portal_url)
