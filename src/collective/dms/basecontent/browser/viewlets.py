bcf66beda6bffef17929e3c136a6d5f7b2a27ef7
Cédric Messiant <cedricmessiant@ecreall.com>
2015-03-19 16:09:50 +0100

    Remove useless tasks viewlets

--- src/collective/dms/basecontent/browser/viewlets.py
+++ src/collective/dms/basecontent/browser/viewlets.py
@@ -67,42 +67,6 @@ class AppendixViewlet(BaseViewlet):
                 'sort_order': 'ascending'}
 
 
-class TasksViewlet(BaseViewlet):
-    grok.name('dms.tasks')
-    grok.order(30)
-    portal_type = 'task'
-    label = _(u"Tasks")
-    noresult_message = _(u"There is no task for this document.")
-    __table__ = TasksTable
-
-
-class OpinionsViewlet(BaseViewlet):
-    grok.name('dms.opinions')
-    grok.order(40)
-    portal_type = 'opinion'
-    label = _(u"Opinion applications")
-    noresult_message = _(u"There is no opinion applications for this document.")
-    __table__ = TasksTable
-
-
-class ValidationsViewlet(BaseViewlet):
-    grok.name('dms.validations')
-    grok.order(50)
-    portal_type = 'validation'
-    label = _(u"Validation applications")
-    noresult_message = _(u"There is no validation applications for this document.")
-    __table__ = TasksTable
-
-
-class InformationsViewlet(BaseViewlet):
-    grok.name('dms.informations')
-    grok.order(60)
-    portal_type = 'information'
-    label = _(u"Informations")
-    noresult_message = _(u"There is no informations for this document.")
-    __table__ = InformationsTable
-
-
 class ChangeTitleViewlet(grok.Viewlet):
     grok.name('dms.changetitle')
     grok.context(Interface)
