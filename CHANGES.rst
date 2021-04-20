752ccbad4b6f9a67ac572d2ec19fdf2c5043aba4
Stephan Geulette <s.geulette@imio.be>
2021-04-20 13:47:59 +0200

    Preparing release 1.6

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-1.6 (unreleased)
+1.6 (2021-04-20)
 ----------------
 
 - RelatedDocs field can handle object_provides criteria.
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #! -*- coding: utf8 -*-
 from setuptools import setup, find_packages
 
-version = '1.6.dev0'
+version = '1.6'
 
 long_description = (
     open('README.rst').read()
