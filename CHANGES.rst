ce69f4381caef7c8ae59450ceb69a2d8543c942f
Stephan Geulette <s.geulette@imio.be>
2022-07-01 11:38:41 +0200

    Preparing release 1.8

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-1.8 (unreleased)
+1.8 (2022-07-01)
 ----------------
 
 - Corrected UnicodeEncodeError.
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #! -*- coding: utf8 -*-
 from setuptools import setup, find_packages
 
-version = '1.8.dev0'
+version = '1.8'
 
 long_description = (
     open('README.rst').read()
