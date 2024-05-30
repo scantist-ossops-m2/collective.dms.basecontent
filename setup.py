3f45c18e6848feaa15ff160cadbbbfdafc103c03
fngaha <franck.o.ngaha@gmail.com>
2024-05-30 11:12:16 +0200

    Add collective.externaleditor 1.0.3

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ setup(
     install_requires=[
         'setuptools',
         'collective.documentviewer',
+        'collective.externaleditor',
         'dexterity.localrolesfield',
         'future',
         'imio.helpers>=0.42',
